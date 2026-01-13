e42bb11f1d73dc26b39b59609717061d52df0ee0
Sutharshan k <maxilo1984@gmail.com>
2026-01-13 12:48:58 +0530

    Add/Update Bento Card source code

---
+++ bentocard/src/App.tsx
@@ -0,0 +1,9 @@
+import { HackathonCard } from "./components/HackathonCard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen w-full bg-gray-100 flex items-center justify-center p-8">
+      <HackathonCard />
+    </div>
+  );
+}
---
+++ bentocard/src/imports/Group3.tsx
@@ -0,0 +1,104 @@
+import svgPaths from "./svg-wtru0vsze2";
+
+function Svg() {
+  return (
+    <div className="absolute contents left-[39.5px] top-[515px]" data-name="svg">
+      <div className="absolute bg-black h-[85px] left-[39.5px] rounded-[32px] top-[515px] w-[481px]" />
+      <div className="absolute bg-[rgba(217,217,217,0.34)] blur-[7px] filter h-[27px] left-[68.5px] opacity-[0.87] rounded-[10px] top-[525px] w-[411px]" />
+      <div className="absolute bg-[rgba(217,217,217,0.66)] blur filter h-[42px] left-[492.5px] opacity-[0.91] rounded-[4px] top-[540px] w-[10px]" />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute inset-[8.53%_83.1%_85.02%_7.79%]" data-name="Group">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 50.2702 41.6114">
+        <g id="Group">
+          <path d={svgPaths.p36086800} fill="var(--fill-0, #70C496)" id="Vector" />
+          <path d={svgPaths.p1c1d4300} fill="var(--fill-0, #70C496)" id="Vector_2" />
+          <path d={svgPaths.p34bf8b00} fill="var(--fill-0, #70C496)" id="Vector_3" />
+          <path d={svgPaths.p23ad1f00} fill="var(--fill-0, #70C496)" id="Vector_4" />
+          <path d={svgPaths.p3a55f00} fill="var(--fill-0, #70C496)" id="Vector_5" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Icon() {
+  return (
+    <div className="absolute contents left-[36px] top-[44px]" data-name="icon">
+      <div className="absolute bg-white left-[36px] rounded-[20px] shadow-[0px_4px_11px_1px_rgba(0,0,0,0.21)] size-[64px] top-[44px]" />
+      <Group />
+    </div>
+  );
+}
+
+function Layer() {
+  return (
+    <div className="absolute contents inset-[6.82%_81.88%_83.26%_6.52%]" data-name="Layer 1">
+      <Icon />
+    </div>
+  );
+}
+
+function Onlinebutton() {
+  return (
+    <div className="absolute contents left-[36px] top-[239px]" data-name="onlinebutton">
+      <div className="absolute bg-[#d9d9d9] h-[28px] left-[36px] rounded-[10px] top-[239px] w-[82px]" />
+      <p className="absolute font-['Arial:Regular',sans-serif] leading-[normal] left-[58px] not-italic text-[14px] text-black text-nowrap top-[245px] tracking-[-0.56px]">Online</p>
+    </div>
+  );
+}
+
+function Group1() {
+  return (
+    <div className="absolute contents left-[439px] top-[63.56px]">
+      <div className="absolute bg-white h-[44.444px] left-[439px] rounded-bl-[18px] rounded-br-[18px] top-[63.56px] w-[64px]" />
+    </div>
+  );
+}
+
+function Date() {
+  return (
+    <div className="absolute contents left-[439px] top-[63.56px]" data-name="date">
+      <Group1 />
+      <p className="absolute font-['Helvetica:Regular',sans-serif] leading-[normal] left-[458px] not-italic text-[24px] text-black text-nowrap top-[72px] tracking-[-0.96px]">20</p>
+    </div>
+  );
+}
+
+export default function Group2() {
+  return (
+    <div className="relative size-full">
+      <div className="absolute bg-white h-[645px] left-0 rounded-[57px] shadow-[0px_7px_30px_0px_rgba(0,0,0,0.25)] top-0 w-[552px]" />
+      <Svg />
+      <p className="absolute font-['Helvetica:Regular',sans-serif] leading-[normal] left-[186px] not-italic opacity-[0.63] text-[32px] text-nowrap text-white top-[539px] tracking-[-1.28px]">Refer Website</p>
+      <div className="absolute flex h-[1.992px] items-center justify-center left-[calc(50%+0.5px)] top-[463px] translate-x-[-50%] w-[481px]" style={{ "--transform-inner-width": "0", "--transform-inner-height": "0" } as React.CSSProperties}>
+        <div className="flex-none rotate-[0.237deg]">
+          <div className="h-0 relative w-[481.004px]">
+            <div className="absolute inset-[-3px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 481.004 3">
+                <line id="Line 1" stroke="var(--stroke-0, #D9D9D9)" strokeWidth="3" x2="481.004" y1="1.5" y2="1.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Layer />
+      <p className="absolute font-['Helvetica:Regular',sans-serif] leading-[normal] left-[35px] not-italic text-[#575757] text-[32px] text-nowrap top-[408px] tracking-[-1.28px]">Prize : $1K</p>
+      <p className="absolute font-['Helvetica:Regular',sans-serif] leading-[normal] left-[424px] not-italic text-[#575757] text-[20px] text-nowrap top-[420px] tracking-[-0.8px]">IIT,Bombay</p>
+      <p className="absolute font-['Helvetica:Regular',sans-serif] leading-[normal] left-[36px] not-italic text-[#575757] text-[36px] text-nowrap top-[180px] tracking-[-1.44px]">Hack2Tech Hackathon</p>
+      <Onlinebutton />
+      <Date />
+      <div className="absolute h-[19.009px] left-[439px] top-[45px] w-[64px]" data-name="Subtract">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 64 19.0088">
+          <path d={svgPaths.p287c680} fill="var(--fill-0, #070707)" id="Subtract" />
+        </svg>
+      </div>
+      <p className="absolute font-['Helvetica:Regular',sans-serif] leading-[normal] left-[460px] not-italic text-[11px] text-nowrap text-white top-[49px] tracking-[0.22px]">JAN</p>
+      <div className="absolute bg-[rgba(31,31,31,0)] border-[#2b2b2b] border-[3px] border-solid h-[85px] left-[39px] rounded-[32px] top-[515px] w-[482px]" />
+    </div>
+  );
+}
