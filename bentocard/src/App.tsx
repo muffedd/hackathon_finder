1b39190e99d45f52de2eb471d404c663636bbef5
NAVIN V <vnavin7714@gmail.com>
2026-01-23 01:20:24 +0530

    cleanup

--- bentocard/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { HackathonCard } from "./components/HackathonCard";
-
-export default function App() {
-  return (
-    <div className="min-h-screen w-full bg-gray-100 flex items-center justify-center p-8">
-      <HackathonCard />
-    </div>
-  );
-}
